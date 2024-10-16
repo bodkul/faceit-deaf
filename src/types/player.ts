28af20abdb340eaa3bb323fff60ea8d1537e27ff
bodkul <bodicetop@gmail.com>
2024-10-17 00:24:40 +0200

    feat: introduce Player interface for Faceit data structure

---
+++ src/types/player.ts
@@ -0,0 +1,12 @@
+export type Player = {
+  player_id: string;
+  nickname: string;
+  avatar: string;
+  games: {
+    cs2: {
+      skill_level: number;
+      faceit_elo: number;
+    };
+  };
+  faceit_url: string;
+};

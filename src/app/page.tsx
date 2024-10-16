e390de0acaecd24a4e9a8194cde25ce5a49e69ba
bodkul <bodicetop@gmail.com>
2024-10-17 01:47:45 +0200

    feat: add header for FACEIT DEAF

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,67 +26,77 @@ export default async function Home() {
 
   return (
     <main className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              {columns.map(({ key, label }) => (
-                <TableHead key={key} className="w-[100px]">
-                  {label}
-                </TableHead>
-              ))}
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {players?.length ? (
-              players.map((player, index) => (
-                <TableRow key={player.id} className="h-[49px]">
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>
-                    {player.avatar && (
-                      <Image
-                        src={player.avatar}
-                        className="w-8 h-8 rounded-full"
-                        width={32}
-                        height={32}
-                        alt={`Avatar of ${player.nickname}`}
-                      />
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    <a
-                      className="font-medium"
-                      href={player.faceit_url.replace("{lang}", "ru")}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {player.nickname}
-                    </a>
-                  </TableCell>
-                  <TableCell>
-                    <Image
-                      src={`/icons/faceit/levels/${player.skill_level}.svg`}
-                      className="w-6 h-6"
-                      width={24}
-                      height={24}
-                      alt={`Skill level ${player.skill_level}`}
-                    />
-                  </TableCell>
-                  <TableCell>{player.faceit_elo}</TableCell>
-                </TableRow>
-              ))
-            ) : (
+      <div className="flex flex-col space-y-20 items-center">
+        <div className="flex flex-col text-center items-center">
+          <a href="/">
+            <div className="font-black text-6xl">FACEIT DEAF</div>
+          </a>
+          <span className="text-2xl">
+            View your FACEIT CS2 performance for deaf Ukrainians.
+          </span>
+        </div>
+        <div className="flex rounded-md border">
+          <Table>
+            <TableHeader>
               <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No players.
-                </TableCell>
+                {columns.map(({ key, label }) => (
+                  <TableHead key={key} className="w-[100px]">
+                    {label}
+                  </TableHead>
+                ))}
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {players?.length ? (
+                players.map((player, index) => (
+                  <TableRow key={player.id} className="h-[49px]">
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>
+                      {player.avatar && (
+                        <Image
+                          src={player.avatar}
+                          className="w-8 h-8 rounded-full"
+                          width={32}
+                          height={32}
+                          alt={`Avatar of ${player.nickname}`}
+                        />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <a
+                        className="font-medium"
+                        href={player.faceit_url.replace("{lang}", "ru")}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {player.nickname}
+                      </a>
+                    </TableCell>
+                    <TableCell>
+                      <Image
+                        src={`/icons/faceit/levels/${player.skill_level}.svg`}
+                        className="w-6 h-6"
+                        width={24}
+                        height={24}
+                        alt={`Skill level ${player.skill_level}`}
+                      />
+                    </TableCell>
+                    <TableCell>{player.faceit_elo}</TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No players.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </main>
   );

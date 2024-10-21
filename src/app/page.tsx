3c7bf39032855cd86b2ad9da179b50aa6ca9e21a
bodkul <bodicetop@gmail.com>
2024-10-21 22:56:49 +0200

    feat: added cursor-default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Leaderboard from "./components/leaderboard";
 
 export default function Home() {
   return (
-    <main className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <main className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 cursor-default">
       <div className="flex flex-col space-y-20 items-center">
         <div className="flex flex-col text-center items-center">
           <a href="/">

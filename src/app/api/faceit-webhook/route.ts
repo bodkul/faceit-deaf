37167ac4be4ff1be1a6baf27f7b1cafbaffcdc32
bodkul <bodicetop@gmail.com>
2024-10-25 22:44:06 +0200

    wip: added logger for wehbook

--- src/app/api/faceit-webhook/route.ts
+++ src/app/api/faceit-webhook/route.ts
@@ -17,6 +17,8 @@ export const fetchCache = "force-no-store";
 export async function POST(req: NextRequest) {
   const { event, payload }: RequestBody = await req.json();
 
+  console.log(payload);
+
   if (event === "match_status_finished") {
     await handleMatchFinished(payload);
   }

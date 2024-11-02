356e7a1e3f91438d52753ed783dafc6573f95908
bodkul <bodicetop@gmail.com>
2024-11-02 21:16:59 +0100

    refactor: uppercase title

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -24,8 +24,8 @@ export default function Layout({
     <main className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 cursor-default">
       <div className="flex flex-col space-y-20 items-center">
         <div className="flex flex-col text-center items-center">
-          <Link href="/" className="font-black text-6xl">
-            FACEIT DEAF
+          <Link href="/" className="font-black text-6xl uppercase">
+            Faceit Deaf
           </Link>
           <span className="text-2xl">
             View your FACEIT CS2 performance for deaf Ukrainians.

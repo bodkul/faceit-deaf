3b3ccd576d60a5249b5871afeebfe5603c684c59
bodkul <bodicetop@gmail.com>
2024-11-03 02:48:34 +0100

    fix: not displayed if there are no items

--- src/app/@twitchBroadcasts/loading.tsx
+++ src/app/@twitchBroadcasts/loading.tsx
@@ -1,10 +1,22 @@
-import { CarouselItem } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <CarouselItem>
-      <Skeleton className="h-[360px] w-[640px]" />
-    </CarouselItem>
+    <Carousel>
+      <CarouselContent className="h-[360px] w-[640px]">
+        <CarouselItem>
+          <Skeleton className="h-[360px] w-[640px]" />
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
--- src/app/@twitchBroadcasts/page.tsx
+++ src/app/@twitchBroadcasts/page.tsx
@@ -1,4 +1,10 @@
-import { CarouselItem } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 interface TwitchStream {
   id: string;
@@ -11,14 +17,26 @@ export default async function Page() {
   const response = await fetch("https://faceitdeaf.pro/api/twitch");
   const twitchPlayers: TwitchStream[] = await response.json();
 
-  return twitchPlayers.map((channel) => (
-    <CarouselItem key={`channel_${channel.user_name}`}>
-      <iframe
-        src={`https://player.twitch.tv/?channel=${channel.user_name}&parent=faceitdeaf.pro`}
-        height="360"
-        width="640"
-        allowFullScreen
-      ></iframe>
-    </CarouselItem>
-  ));
+  if (!twitchPlayers.length) {
+    return;
+  }
+
+  return (
+    <Carousel>
+      <CarouselContent className="h-[360px] w-[640px]">
+        {twitchPlayers.map((channel) => (
+          <CarouselItem key={`channel_${channel.user_name}`}>
+            <iframe
+              src={`https://player.twitch.tv/?channel=${channel.user_name}&parent=faceitdeaf.pro`}
+              height="360"
+              width="640"
+              allowFullScreen
+            ></iframe>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
 }

39ae13b668d38da24746b865cf7aca245bc7be0f
bodkul <bodicetop@gmail.com>
2024-11-27 21:07:25 +0100

    chore: clean up redundant console logs

--- src/app/api/faceit-webhook/route.ts
+++ src/app/api/faceit-webhook/route.ts
@@ -8,14 +8,6 @@ export async function POST(req: NextRequest) {
   const body: MatchStatusEvent = await req.json();
 
   console.log(body);
-  console.log(body.payload);
-  console.log(body.payload.teams);
-  console.log(body.payload.teams[0]);
-  console.log(body.payload.teams[1]);
-  console.log([
-    ...body.payload.teams[0].roster,
-    ...body.payload.teams[1].roster,
-  ]);
 
   if (body.event === "match_status_finished") {
     await handleMatchFinished(body.payload);

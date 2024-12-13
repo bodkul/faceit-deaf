3780b7683b8d452b390f6bf924e59dab6dc31915
bodkul <bodicetop@gmail.com>
2024-12-13 16:49:45 +0100

    refactor: optimize upsertPlayers to batch upsert data

--- src/lib/database/players.ts
+++ src/lib/database/players.ts
@@ -2,27 +2,25 @@ import { supabase } from "@/lib/supabaseClient";
 import { PlayerWithStats } from "@/types/api";
 import { logger } from "@/lib/logger";
 
-export async function upsertPlayer(player: PlayerWithStats) {
-  const { error } = await supabase.from("players").upsert({
-    id: player.player_id,
-    avatar: player.avatar,
-    nickname: player.nickname,
-    skill_level: player.games.cs2.skill_level,
-    faceit_elo: player.games.cs2.faceit_elo,
-    faceit_url: player.faceit_url,
-    steam_id_64: player.steam_id_64,
-    matches: player.lifetime.Matches,
-    average_headshots_percent: player.lifetime["Average Headshots %"],
-    average_kd_ratio: player.lifetime["Average K/D Ratio"],
-  });
+export async function upsertPlayers(players: PlayerWithStats[]) {
+  const { error } = await supabase.from("players").upsert(
+    players.map((player) => ({
+      id: player.player_id,
+      avatar: player.avatar,
+      nickname: player.nickname,
+      skill_level: player.games.cs2.skill_level,
+      faceit_elo: player.games.cs2.faceit_elo,
+      faceit_url: player.faceit_url,
+      steam_id_64: player.steam_id_64,
+      matches: player.lifetime.Matches,
+      average_headshots_percent: player.lifetime["Average Headshots %"],
+      average_kd_ratio: player.lifetime["Average K/D Ratio"],
+    })),
+  );
 
   if (error) {
     logger.error(`Failed to upsert players: ${error}`);
   }
-}
-
-export async function upsertPlayers(players: PlayerWithStats[]) {
-  await Promise.all(players.map(upsertPlayer));
 }
 
 export async function getPlayersByIds(playerIds: string[]) {

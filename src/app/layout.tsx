c78bae6b7e961857303e06230df6d4ab04bd9e42
bodkul <bodicetop@gmail.com>
2024-12-13 20:38:41 +0100

    refactor: replace ThemeProvider import with next-themes and remove custom theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import { FaceitIcon } from "./icons";
 import Link from "next/link";

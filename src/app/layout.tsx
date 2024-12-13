dd0f4e0baaf8872ed876e5d2a2294c38b3aef3a1
bodkul <bodicetop@gmail.com>
2024-12-13 20:38:05 +0100

    feat: add header with FaceitIcon and navigation to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { FaceitIcon } from "./icons";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -25,7 +27,24 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`font-sans ${geistSans.variable} antialiased`}>
         <ThemeProvider attribute="class" enableSystem enableColorScheme>
-          {children}
+          <div className="flex-col md:flex max-w-screen-xl mx-auto border-x border-b">
+            <div className="border-b sticky top-0 z-10 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
+              <div className="flex h-14 items-center px-4">
+                <div className="flex mr-4">
+                  <Link
+                    className="flex mr-4 items-center gap-2 lg:mr-6"
+                    href="/"
+                  >
+                    <FaceitIcon className="w-6 h-6 text-[#ff5500] fill-[#ff5500]" />
+                    <span className="inline-block font-bold">
+                      {metadata.title!.toString()}
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <main className="flex-1 space-y-4 p-8 pt-6">{children}</main>
+          </div>
           <Toaster />
         </ThemeProvider>
         <Analytics />

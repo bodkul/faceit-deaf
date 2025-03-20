92245b2964832af532f4c29b80d22c12c9ca8ee1
bodkul <bodicetop@gmail.com>
2025-03-20 18:25:07 +0100

    fix: add pagination for loading

--- src/app/player/[username]/loading.tsx
+++ src/app/player/[username]/loading.tsx
@@ -1,10 +1,23 @@
 import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+} from "@/components/ui/pagination";
 import { Skeleton } from "@/components/ui/skeleton";
 import {
   Table,
@@ -68,20 +81,72 @@ export default function Loading() {
           <CardDescription>0 matches played</CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="flex rounded-md border">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[20%]">Date</TableHead>
-                  <TableHead className="w-[16%]">Map</TableHead>
-                  <TableHead className="w-[16%]">Score</TableHead>
-                  <TableHead className="w-[16%]">K - D</TableHead>
-                  <TableHead className="w-[16%]">K/D</TableHead>
-                  <TableHead className="w-[16%]">Rating 2.0</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>{renderLoadingRows(20)}</TableBody>
-            </Table>
+          <div className="space-y-4">
+            <div className="flex rounded-md border">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[20%]">Date</TableHead>
+                    <TableHead className="w-[16%]">Map</TableHead>
+                    <TableHead className="w-[16%]">Score</TableHead>
+                    <TableHead className="w-[16%]">K - D</TableHead>
+                    <TableHead className="w-[16%]">K/D</TableHead>
+                    <TableHead className="w-[16%]">Rating 2.0</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>{renderLoadingRows(20)}</TableBody>
+              </Table>
+            </div>
+
+            <Pagination>
+              <div className="flex items-center space-x-6 lg:space-x-8">
+                <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+                  Page 1 of 1
+                </div>
+                <PaginationContent className="gap-2">
+                  <PaginationItem>
+                    <Button
+                      variant="outline"
+                      className="hidden h-8 w-8 p-0 lg:flex"
+                      disabled={true}
+                    >
+                      <span className="sr-only">Go to first page</span>
+                      <ChevronsLeft />
+                    </Button>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <Button
+                      variant="outline"
+                      className="h-8 w-8 p-0"
+                      disabled={true}
+                    >
+                      <span className="sr-only">Go to previous page</span>
+                      <ChevronLeft />
+                    </Button>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <Button
+                      variant="outline"
+                      className="h-8 w-8 p-0"
+                      disabled={true}
+                    >
+                      <span className="sr-only">Go to next page</span>
+                      <ChevronRight />
+                    </Button>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <Button
+                      variant="outline"
+                      className="hidden h-8 w-8 p-0 lg:flex"
+                      disabled={true}
+                    >
+                      <span className="sr-only">Go to last page</span>
+                      <ChevronsRight />
+                    </Button>
+                  </PaginationItem>
+                </PaginationContent>
+              </div>
+            </Pagination>
           </div>
         </CardContent>
       </Card>

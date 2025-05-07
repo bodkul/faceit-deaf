f933d461a4f13054d286daaecee0bddc06b48395
bodkul <bodicetop@gmail.com>
2025-05-08 00:45:08 +0200

    rollback: nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg"),

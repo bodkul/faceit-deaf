ac5d735b08203c844d1ec310e35d92b137959502
bodkul <bodicetop@gmail.com>
2025-07-03 19:26:13 +0200

    chore: update package.json and package-lock.json to include p-limit dependency and upgrade yocto-queue refactor: enhance error handling in API fetching functions for better debugging and reliability

--- src/lib/faceit/api/api.ts
+++ src/lib/faceit/api/api.ts
@@ -1,23 +1,46 @@
+import pLimit from "p-limit";
+
 import faceitClient from "@/lib/faceit/client";
 
 import type { Match, MatchStats, Player } from "./types";
 
+const limit = pLimit(5);
+
 export const fetchPlayer = async (playerId: string) => {
-  return await faceitClient<Player>(`players/${playerId}`).json();
+  try {
+    return await faceitClient<Player>(`players/${playerId}`).json();
+  } catch (error) {
+    console.error(`Failed to fetch player with ID ${playerId}:`, error);
+    throw new Error(`Unable to fetch player with ID ${playerId}`);
+  }
 };
 
 export const fetchPlayers = async (playerIds: string[]) => {
-  return await Promise.all(playerIds.map(fetchPlayer));
+  return await Promise.all(
+    playerIds.map((playerId) => limit(() => fetchPlayer(playerId))),
+  );
 };
 
 export const fetchMatch = async (matchId: string) => {
-  return await faceitClient<Match>(`matches/${matchId}`).json();
+  try {
+    return await faceitClient<Match>(`matches/${matchId}`).json();
+  } catch (error) {
+    console.error(`Failed to fetch match with ID ${matchId}:`, error);
+    throw new Error(`Unable to fetch match with ID ${matchId}`);
+  }
 };
 
-export const fetchMatches = async (matchId: string[]) => {
-  return await Promise.all(matchId.map(fetchMatch));
+export const fetchMatches = async (matchIds: string[]) => {
+  return await Promise.all(
+    matchIds.map((matchId) => limit(() => fetchMatch(matchId))),
+  );
 };
 
 export const fetchMatchStats = async (matchId: string) => {
-  return await faceitClient<MatchStats>(`matches/${matchId}/stats`).json();
+  try {
+    return await faceitClient<MatchStats>(`matches/${matchId}/stats`).json();
+  } catch (error) {
+    console.error(`Failed to fetch match stats with ID ${matchId}:`, error);
+    throw new Error(`Unable to fetch match stats with ID ${matchId}`);
+  }
 };
--- src/lib/faceit/client.ts
+++ src/lib/faceit/client.ts
@@ -6,13 +6,11 @@ const faceitClient = ky.create({
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN!}`,
   },
   hooks: {
-    afterResponse: [
-      async (request, options, response) => {
-        if (!response.ok) {
-          console.error(
-            `Error fetching data from ${request.url}: ${response.status} ${response.statusText}`,
-          );
-        }
+    beforeRetry: [
+      ({ request, error, retryCount }) => {
+        console.warn(
+          `Retrying request to ${request.url}. Attempt #${retryCount}. Error: ${error.message}`,
+        );
       },
     ],
   },

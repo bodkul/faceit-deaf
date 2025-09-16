a0360ae23f7c2dc4ca003d721ef4c87f1d125bd1
bodkul <bodicetop@gmail.com>
2025-09-16 23:32:01 +0200

    feat: refactor match status handling and improve task triggering logic

--- src/lib/faceit/handlers.ts
+++ src/lib/faceit/handlers.ts
@@ -1,8 +1,7 @@
-import { tasks } from "@trigger.dev/sdk";
 import pMap from "p-map";
 
 import { fetchMatch } from "@/lib/faceit/api";
-import type { MatchPayload, MatchStatusEvent } from "@/lib/faceit/match-events";
+import type { MatchPayload } from "@/lib/faceit/match-events";
 import {
   deleteMatch,
   getExistingPlayers,
@@ -10,9 +9,8 @@ import {
   upsertMatchTeam,
   upsertMatchTeamPlayers,
 } from "@/lib/supabase/mutations";
-import { syncFinishedMatchTask } from "@/trigger/update-match";
 
-async function handleMatchStatusReady(payload: MatchPayload) {
+export async function handleMatchStatusReady(payload: MatchPayload) {
   const matchId = payload.id.replace(/^1-/, "");
   const playerIds = payload.teams.flatMap((team) =>
     team.roster.map((p) => p.id),
@@ -65,40 +63,7 @@ async function handleMatchStatusReady(payload: MatchPayload) {
   );
 }
 
-async function handleMatchStatusCancelled(payload: MatchPayload) {
+export async function handleMatchStatusCancelled(payload: MatchPayload) {
   const matchId = payload.id.replace(/^1-/, "");
   await deleteMatch(matchId);
-}
-
-export async function handleMatchStatusEvent(body: MatchStatusEvent) {
-  console.info(`Handling match status event: ${body.event}`, body.payload);
-
-  if (
-    body.payload.game !== "cs2" ||
-    body.payload.region !== "EU" ||
-    body.payload.organizer_id !== "faceit" ||
-    body.payload.entity.id !== "f4148ddd-bce8-41b8-9131-ee83afcdd6dd"
-  ) {
-    return;
-  }
-
-  switch (body.event) {
-    case "match_status_ready":
-      await handleMatchStatusReady(body.payload);
-      break;
-    case "match_status_cancelled":
-      await handleMatchStatusCancelled(body.payload);
-      break;
-    case "match_status_finished":
-      try {
-        await tasks.trigger<typeof syncFinishedMatchTask>(
-          "sync-finished-match",
-          body.payload,
-        );
-        console.log("Task triggered successfully");
-      } catch (err) {
-        console.error("Error triggering task", err);
-      }
-      break;
-  }
 }

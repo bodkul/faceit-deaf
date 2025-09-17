90509c17c3c49909124fac92feacc0063c4a75c6
Bohdan Kulichievskyi <bohdan.kulichievskyi@adesso.de>
2025-09-17 11:43:12 +0200

    refactor: clean up code formatting and remove unnecessary comments in configuration

--- trigger.config.ts
+++ trigger.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
   project: "proj_evyvlteecnwipwaujxyv",
   runtime: "node",
   logLevel: "log",
-  // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
-  // You can override this on an individual task.
-  // See https://trigger.dev/docs/runs/max-duration
   maxDuration: 3600,
   retries: {
     enabledInDev: true,

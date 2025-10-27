a66cc0beea058193cc6e8af379ed707d4b670b84
bodkul <bodicetop@gmail.com>
2025-10-27 23:53:19 +0100

    fix: update site name in configuration and adjust welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { siteConfig } from "@/config/site";
+
 import { LiveMatches } from "./_components/LiveMatches";
 
 export default function Page() {
@@ -7,7 +9,8 @@ export default function Page() {
     <>
       <div className="container flex flex-col items-center gap-2 py-8 text-center md:py-16 lg:py-20 xl:gap-4">
         <h1 className="leading-tighter max-w-2xl text-4xl font-semibold tracking-tight text-balance text-primary lg:leading-[1.1] lg:font-semibold xl:text-5xl xl:tracking-tighter">
-          Welcome to faceitdeaf.pro
+          Welcome to <span className="text-orange-500">{siteConfig.name}</span>
+          .pro
         </h1>
         <p className="max-w-3xl text-base text-balance text-foreground sm:text-lg">
           FaceitDeaf is a platform for tracking, analyzing, and watching FACEIT
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "faceitdeaf",
+  name: "csdeaf",
   description:
     "FACEIT CS2 stats and rankings platform for the deaf community. Track your performance, follow the leaderboard, and compare results with other players.",
 };

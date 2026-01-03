e355eb62c02b291f3cf6b8e5157970236b4c96e9
bodkul <bodicetop@gmail.com>
2026-01-03 16:46:11 +0100

    refactor: update MatchType and MatchHistoryType to use "data" for consistency in type definitions

--- src/types/match.ts
+++ src/types/match.ts
@@ -2,14 +2,14 @@ import type { useMatch } from "@/hooks/useMatch";
 import type { useMatchHistory } from "@/hooks/useMatchHistory";
 import type { useRecentMatches } from "@/hooks/useRecentMatches";
 
-export type MatchType = NonNullable<ReturnType<typeof useMatch>["match"]>;
+export type MatchType = NonNullable<ReturnType<typeof useMatch>["data"]>;
 
 export type TeamType = MatchType["teams"][number];
 
 export type PlayerType = TeamType["team_players"][number];
 
 export type MatchHistoryType = NonNullable<
-  ReturnType<typeof useMatchHistory>["matches"]
+  ReturnType<typeof useMatchHistory>["data"]
 >[number];
 
 export type RecentMatchType = NonNullable<

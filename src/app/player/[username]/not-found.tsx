32ff73be7ade237a37f0b0f90be96564a54f7123
bodkul <bodicetop@gmail.com>
2026-02-04 00:29:15 +0100

    fix(notFound): add col-span-full class to improve layout

--- src/app/player/[username]/not-found.tsx
+++ src/app/player/[username]/not-found.tsx
@@ -1,6 +1,6 @@
 export default function notFound() {
   return (
-    <div className="flex h-96 flex-col items-center justify-center text-center">
+    <div className="col-span-full flex h-96 flex-col items-center justify-center text-center">
       Sorry, the player was not found 😔
     </div>
   );

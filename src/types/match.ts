6bdb57c9ae9c0185ef415c2f897c75b2b806fe2f
bodkul <bodicetop@gmail.com>
2026-02-04 01:04:25 +0100

    refactor(match): remove unused MatchHistoryType type definition

--- src/types/match.ts
+++ src/types/match.ts
@@ -1,5 +1,4 @@
 import type { useMatch } from "@/hooks/useMatch";
-import type { useMatchHistory } from "@/hooks/useMatchHistory";
 import type { useRecentMatches } from "@/hooks/useRecentMatches";
 
 export type MatchType = NonNullable<ReturnType<typeof useMatch>["data"]>;
@@ -7,10 +6,6 @@ export type MatchType = NonNullable<ReturnType<typeof useMatch>["data"]>;
 export type TeamType = MatchType["teams"][number];
 
 export type PlayerType = TeamType["team_players"][number];
-
-export type MatchHistoryType = NonNullable<
-  ReturnType<typeof useMatchHistory>["data"]
->[number];
 
 export type RecentMatchType = NonNullable<
   ReturnType<typeof useRecentMatches>["data"]
